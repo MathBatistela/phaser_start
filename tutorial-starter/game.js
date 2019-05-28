74998c294fc0806dc0374ef4462e7e7e8b573fac
MathBatistela <matheusbatistela@alunos.utfpr.edu.br>
2019-05-28 16:45:08 -0300

    att: 2019-05-28 16:45

--- tutorial-starter/game.js
+++ tutorial-starter/game.js
@@ -3,8 +3,8 @@
 /* Game code goes here! */
 
 var VALUES = {
-    SHIP_ACCELERATION: 1300,
-    SHIP_MAX_VELOCITY: 400
+    SHIP_ACCELERATION:  1300,
+    SHIP_MAX_VELOCITY:  400
 }
 
 var config = {
@@ -13,12 +13,13 @@ var config = {
         mode: Phaser.Scale.FIT,
         parent: 'game-container',
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1280, // HD
-        height: 720
+        width: 1920, // FULL HD
+        height: 1080
     },
     physics: {
         default: 'arcade',
         arcade: {
+            //gravity: { y: 300 },
             debug: false
         }
     },
@@ -41,33 +42,66 @@ var enemies
 var enemyBullets
 var explosions
 var asteroids
+// backgrounds
+var bgStars
+var parallax
+// logic
+var launchCounter
+var launchTime
 
 function preload() {
     this.load.spritesheet('ship', 'assets/ship_124x48.png',
-        { frameWidth: 124, frameHeight: 48 })
+            { frameWidth: 124, frameHeight: 48 } )
     this.load.spritesheet('bullet', 'assets/bullet_20x21.png',
-        { frameWidth: 20, frameHeight: 21 })
+            { frameWidth: 20, frameHeight: 21 } )
     this.load.spritesheet('bullet-enemy', 'assets/bullet-enemy_20x21.png',
-        { frameWidth: 20, frameHeight: 21 })
+            { frameWidth: 20, frameHeight: 21 } )
     this.load.spritesheet('enemy', 'assets/enemy2_64x73.png',
-        { frameWidth: 64, frameHeight: 73 })
+            { frameWidth: 64, frameHeight: 73 } )
     this.load.spritesheet('explosion', 'assets/explosion_31x31.png',
-        { frameWidth: 31, frameHeight: 31 })
+            { frameWidth: 31, frameHeight: 31 } )
 
     this.load.image('cannon', 'assets/cannon.png')
     this.load.image('target', 'assets/target.png')
     this.load.image('asteroid', 'assets/meteor.png')
 
+    this.load.image('stars', 'assets/background1.png')
+    this.load.image('parallax', 'assets/background0.png')
+}
+
+function createBackground(context, speed, scale, tint) {
+    var bg = context.add.tileSprite(
+        game.renderer.width/2,
+        game.renderer.height-240/2*scale, 
+        game.renderer.width,
+        240*scale, 'parallax')
+    bg.scrollSpeed = speed
+    bg.tileScaleY = scale
+    bg.setTint(tint)
+    return bg
 }
 
 function create() {
+    launchCounter = 0
+    launchTime = 120
+
+    bgStars = this.add.tileSprite(
+        game.renderer.width/2, game.renderer.height/2,
+        game.renderer.width, game.renderer.height, 'stars')
+    bgStars.fxCounter = 0
+    
+    parallax = []
+    parallax[0] = createBackground(this, 5, 2.4, 0xAAAAAA)
+    parallax[1] = createBackground(this, 15, 1.5, 0xFFFFFF)
+    parallax[2] = createBackground(this, 20, 0.3, 0x888888)
+
     ship = this.physics.add.sprite(
-        game.renderer.width / 2, game.renderer.height / 2, 'ship')
+        game.renderer.width/2, game.renderer.height/2, 'ship')
     ship.setDrag(VALUES.SHIP_ACCELERATION)
     ship.setMaxVelocity(VALUES.SHIP_MAX_VELOCITY)
     ship.setScale(0.75)
-    ship.setSize(ship.width - 40, ship.height - 30)
-    ship.setOffset(30, 20)
+    ship.setSize(ship.width-40, ship.height-30)
+    ship.setOffset(30,20)
     ship.hitCounter = 0
 
     cannon = this.physics.add.sprite(ship.x, ship.y, 'cannon')
@@ -75,17 +109,38 @@ function create() {
     cannon.shotCounter = 0
     cannon.shotRate = 3
 
-    asteroids = this.physics.add.group()
-    for (var i =0; i < 5; i++) {
-        var x = Phaser.Math.Between(100, game.renderer.width-100)
-        var ast = asteroids.create()
-    }
-
     target = this.physics.add.sprite(ship.x, ship.y, 'target')
+
+    asteroids = this.physics.add.group()
+    for (var i = 0; i < 5; i++) {
+        var x = Phaser.Math.Between(100, game.renderer.width-100)
+        var y = Phaser.Math.Between(100, game.renderer.height-100)
+        var ast = asteroids.create(x, y, 'asteroid')
+        ast.setVelocity(Phaser.Math.Between(-90, -30),
+                        Phaser.Math.Between(-50, 50))
+        ast.setScale(Phaser.Math.FloatBetween(0.5, 0.8))
+        ast.setAngle(Phaser.Math.FloatBetween(-180, 180))
+        //ast.setCircle(ast.width/2-20, 20, 20)
+        ast.setSize(130,130)
+        ast.setOffset(35,35)
+        ast.setImmovable(true)
+    }
 
     bullets = this.physics.add.group({
         maxSize: 20,
         defaultKey: 'bullet'
+    })
+    enemyBullets = this.physics.add.group({
+        maxSize: 10,
+        defaultKey: 'bullet-enemy'
+    })
+    enemies = this.physics.add.group({
+        maxSize: 30,
+        defaultKey: 'enemy'
+    })
+    explosions = this.physics.add.group({
+        maxSize: 30,
+        defaultKey: 'explosion'
     })
 
     this.anims.create({
@@ -111,8 +166,8 @@ function create() {
 
     this.anims.create({
         key: 'bullet-idle',
-        frames: this.anims.generateFrameNumbers('bullet',
-            { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('bullet', 
+                        { start:0, end: 3 }),
         frameRate: 20,
         repeat: -1,
         yoyo: true
@@ -128,15 +183,15 @@ function create() {
 
     cursors = this.input.keyboard.createCursorKeys()
 
-    this.input.on('pointerdown', function (pointer) {
+    this.input.on('pointerdown', function(pointer) { 
         cannon.isShooting = true
     }, this)
 
-    this.input.on('pointerup', function (pointer) {
+    this.input.on('pointerup', function(pointer) { 
         cannon.isShooting = false
     }, this)
 
-    this.input.on('pointermove', function (pointer) {
+    this.input.on('pointermove', function(pointer) { 
         target.x = pointer.x
         target.y = pointer.y
     }, this)
@@ -147,6 +202,12 @@ function firePlayerBullet(context) {
         900, 'bullet-idle')
 }
 
+function fireEnemyBullet(context, enemy) {
+    var rotation = Phaser.Math.Angle.BetweenPoints(enemy, ship)
+    fireBullet(context, enemyBullets, enemy.x, enemy.y, rotation,
+        250, 'bullet-enemy-idle')
+}
+
 function fireBullet(context, group, x, y, rotation, speed, anim) {
     var bullet = group.get()
     if (!bullet) return
@@ -155,30 +216,43 @@ function fireBullet(context, group, x, y, rotation, speed, anim) {
     var velocity = new Phaser.Math.Vector2()
     context.physics.velocityFromRotation(rotation, speed, velocity)
     bullet.setVelocity(velocity.x, velocity.y)
-    //bullet.anims.play(anim)
+    bullet.anims.play(anim)
+}
+
+function createExplosion(x, y, scale=1) {
+    var exp = explosions.get()
+    if (!exp)
+        return
+
+    exp.enableBody(true, x, y, true, true)
+    exp.setScale(scale)
+    exp.anims.play('explosion-idle')
+    exp.on('animationcomplete', function(anim, frame, target) {
+        target.disableBody(true, true)
+    }, this)
 }
 
 function updatePlayer(context) {
     if (!ship.active)
-        return
-
+    return
+    
     if (cursors.left.isDown) {
         ship.setAccelerationX(-VALUES.SHIP_ACCELERATION)
     } else
-        if (cursors.right.isDown) {
-            ship.setAccelerationX(VALUES.SHIP_ACCELERATION)
-        } else {
-            ship.setAccelerationX(0)
-        }
-
+    if (cursors.right.isDown) {
+        ship.setAccelerationX(VALUES.SHIP_ACCELERATION)
+    } else {
+        ship.setAccelerationX(0)
+    }
+    
     if (cursors.up.isDown) {
         ship.setAccelerationY(-VALUES.SHIP_ACCELERATION)
     } else
-        if (cursors.down.isDown) {
-            ship.setAccelerationY(VALUES.SHIP_ACCELERATION)
-        } else {
-            ship.setAccelerationY(0)
-        }
+    if (cursors.down.isDown) {
+        ship.setAccelerationY(VALUES.SHIP_ACCELERATION)
+    } else {
+        ship.setAccelerationY(0)
+    }
 
     // context = this
     context.physics.world.wrap(ship)
@@ -199,14 +273,129 @@ function updateBullet(context, bullet) {
         return
 
     if (!Phaser.Geom.Rectangle.Overlaps(
-        context.physics.world.bounds, bullet.getBounds())) {
+            context.physics.world.bounds, bullet.getBounds())) {
         bullet.disableBody(true, true)
     }
 }
 
-function update() {
-    updatePlayer(this)
-    for (var bullet of bullets.getChildren()) {
-        updateBullet(this, bullet)
+function launchEnemy(context) {
+    launchCounter++
+    if (launchCounter < launchTime)
+        return
+    
+    // it's time to launch an enemy!
+    launchCounter = 0
+    var enemy = enemies.get()
+    if (!enemy) 
+        return
+
+    var x = Phaser.Math.Between(1, game.renderer.width-1)
+    var y = Phaser.Math.Between(1, game.renderer.height-1)
+    enemy.enableBody(true, x, y, true, true)
+    enemy.body.enable = false
+    enemy.anims.play('enemy-idle')
+    enemy.setTint(0xffffff)
+    enemy.setScale(2)
+    enemy.alpha = 0
+    enemy.dirCounter = 45
+    enemy.ready = false
+    enemy.health = 5
+    enemy.hitCounter = 0
+    enemy.attackCounter = 0
+
+    context.tweens.add({
+        targets: enemy,
+        scaleX: 1,
+        scaleY: 1,
+        alpha: 0.5,
+        ease: 'Power1',
+        duration: 3000,
+        onComplete: onEnemyStart
+    })
+}
+
+function onEnemyStart(tween, targets) {
+    var enemy = targets[0]
+    enemy.alpha = 1
+    enemy.ready = true
+    enemy.body.enable = true
+}
+
+function updateEnemy(context, enemy) {
+    if (!enemy.active || !enemy.ready)
+        return
+
+    enemy.hitCounter--
+    if (enemy.hitCounter <= 0)
+        enemy.setTint(0xffffff)
+
+    // attack logic
+    enemy.attackCounter++
+    if (enemy.attackCounter >= 180) {
+        enemy.attackCounter = 0
+        fireEnemyBullet(context, enemy)
+    }
+
+    // direction change logic
+    enemy.dirCounter++
+    if (enemy.dirCounter >= 45) {
+        enemy.dirCounter = 0
+        var velocity = new Phaser.Math.Vector2()
+        var rotation = Phaser.Math.Between(-180,180) * Phaser.Math.DEG_TO_RAD
+        context.physics.velocityFromRotation(rotation, 80, velocity)
+        enemy.setVelocity(velocity.x, velocity.y)
+    }
+    context.physics.world.wrap(enemy, enemy.width/2)
+}
+
+function updateBackgrounds() {
+    bgStars.tileScaleX = 1 + Math.cos(bgStars.fxCounter) * 0.2
+    bgStars.tileScaleY = 1 + Math.sin(bgStars.fxCounter) * 0.2
+    bgStars.fxCounter += 0.01
+
+    for (var bg of parallax) {
+        bg.tilePositionX += bg.scrollSpeed
     }
 }
+
+function update() {
+    updateBackgrounds()
+    updatePlayer(this)
+    launchEnemy(this)
+    for (var bullet of bullets.getChildren()){
+        updateBullet(this, bullet)
+    }
+    for (var bullet of enemyBullets.getChildren()){
+        updateBullet(this, bullet)
+    }
+    for (var enemy of enemies.getChildren()){
+        updateEnemy(this, enemy)
+    }
+
+    this.physics.world.wrap(asteroids, 50)
+    this.physics.world.collide(ship, asteroids)
+    this.physics.world.collide(enemies, asteroids)
+
+    this.physics.world.overlap(bullets, asteroids, hitAsteroids, null, this)
+    this.physics.world.overlap(enemyBullets, asteroids, hitAsteroids, null, this)
+    
+    this.physics.world.overlap(bullets, enemies, hitEnemy, null, this)
+}
+
+function hitAsteroids(bullet, asteroid) {
+    createExplosion(bullet.x, bullet.y)
+    bullet.disableBody(true, true)
+}
+
+function hitEnemy(bullet, enemy) {
+    createExplosion(bullet.x, bullet.y)
+    bullet.disableBody(true, true)
+
+    enemy.hitCounter = 7
+    enemy.setTint(0xff0000)
+    enemy.health--
+    if (enemy.health <= 0) {
+        createExplosion(enemy.x, enemy.y, 2)
+        enemy.disableBody(true, true)
+    }
+} 
